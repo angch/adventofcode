b849159751d1762972819a4a0bd6b2b006e46015
Ang Chin Han <angch@tuxuri.com>
2020-12-07 17:10:33 +0800

    chore: Cleanups for 2020-07/angch

--- 2020-07/angch/main.go
+++ 2020-07/angch/main.go
@@ -8,11 +8,6 @@ import (
 	"strings"
 )
 
-type Foo struct {
-	Num  int
-	Item string
-}
-
 func do(fileName string) (int, int) {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -20,70 +15,48 @@ func do(fileName string) (int, int) {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	rules := make(map[string][]Foo)
-	rules2 := make(map[string]map[string]int)
+	rules := make(map[string]map[string]int)
 
 	for scanner.Scan() {
 		l := scanner.Text()
-
 		l = strings.ReplaceAll(l, " bags", "")
 		l = strings.ReplaceAll(l, " bag", "")
 		l = strings.ReplaceAll(l, ".", "")
 		sp := strings.Split(l, " contain ")
 
-		left := sp[0]
 		right := strings.Split(sp[1], ", ")
 
-		items := make([]Foo, 0)
-		items2 := make(map[string]int)
+		items := make(map[string]int)
 		for _, v := range right {
 			if v == "no other" {
 				continue
 			}
-			num := 0
-			s1, s2 := "", ""
+			num, s1, s2 := 0, "", ""
 			fmt.Sscanf(v, "%d %s %s", &num, &s1, &s2)
-			s := s1 + " " + s2
-
-			items = append(items, Foo{num, s})
-			items2[s] = num
+			items[s1+" "+s2] = num
 		}
-
-		rules[left] = items
-		rules2[left] = items2
+		rules[sp[0]] = items
 	}
 
 	canContain := make(map[string]int)
 	canContain["shiny gold"] = 1
 a:
-	for k, v := range rules2 {
-		c := false
+	for k, v := range rules {
 		for k2, v2 := range canContain {
-			if v[k2] >= v2 {
-				c = true
+			if v[k2] >= v2 && canContain[k] == 0 {
+				canContain[k] = v2
+				goto a
 			}
 		}
-		if c && canContain[k] == 0 {
-			canContain[k] = 1
-			goto a
-		}
 	}
-
-	log.Println(rules, len(canContain)-1)
-
-	count := countme(rules, "shiny gold")
-	log.Println("Count ", count-1)
-
-	return 0, 0
+	return len(canContain) - 1, countme(rules, "shiny gold") - 1
 }
 
-func countme(rules map[string][]Foo, c string) int {
+func countme(rules map[string]map[string]int, c string) int {
 	count := 0
-	for _, v := range rules[c] {
-		count += v.Num * countme(rules, v.Item)
-		log.Println(c, "contains", v.Num, v.Item, count)
+	for k, v := range rules[c] {
+		count += v * countme(rules, k)
 	}
-	log.Println(" ", c, "contains ", count+1)
 	return count + 1
 }
 
