404119598618647bd4b80513786db8f2a197c41d
Ang Chin Han <angch@tuxuri.com>
2020-12-16 01:33:02 +0800

    feat(2020-05/angch): More optimization

--- 2020-15/angch/main.go
+++ 2020-15/angch/main.go
@@ -72,9 +72,7 @@ type Vec struct {
 	Two int32
 }
 
-func do2(input []int) (int, int) {
-	ret1, ret2 := 0, 0
-
+func do2(input []int) (ret1 int, ret2 int) {
 	nums := make([]Vec, 30000000)
 	last, pos := int32(0), int32(0)
 	for _, v := range input {
@@ -83,9 +81,8 @@ func do2(input []int) (int, int) {
 		nums[last] = Vec{One: int32(pos)}
 	}
 
-	// k := nums[last]
+	k := nums[last]
 	for {
-		k := nums[last]
 		if k.Two == 0 {
 			last = 0
 		} else {
@@ -101,29 +98,52 @@ func do2(input []int) (int, int) {
 
 		k = nums[last]
 		if k.One == 0 {
-			nums[last] = Vec{
+			k = Vec{
 				One: pos,
 			}
 		} else if k.Two == 0 {
-			nums[last] = Vec{
+			k = Vec{
 				One: pos - k.One,
 				Two: pos,
 			}
 		} else {
-			nums[last] = Vec{
+			k = Vec{
 				One: pos - k.Two,
 				Two: pos,
 			}
 		}
+		nums[last] = k
 	}
 	ret2 = int(last)
+	return
+}
 
-	return ret1, ret2
+func do3(input []int) (ret1 int, ret2 int) {
+	nums := make([]int32, 30000000)
+	last, pos := int32(0), int32(0)
+	for _, v := range input {
+		pos++
+		last = int32(v)
+		nums[last] = pos
+	}
+
+	for prev := int32(0); pos < 30000000; pos++ {
+		if pos == 2020 {
+			ret1 = int(last)
+		}
+		prev, nums[last] = nums[last], pos
+		if prev == 0 {
+			last = 0
+		} else {
+			last = pos - prev
+		}
+	}
+	return ret1, int(last)
 }
 
 func main() {
 	// log.Println(do("test.txt"))
 	// log.Println(do("test2.txt"))
 	// log.Println(do("input.txt"))
-	log.Println(do2([]int{12, 20, 0, 6, 1, 17, 7}))
+	log.Println(do3([]int{12, 20, 0, 6, 1, 17, 7}))
 }
