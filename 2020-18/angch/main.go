52ddcccf8619ddc397f6cc8b0b1f450114a732fb
Ang Chin Han <angch@tuxuri.com>
2020-12-18 17:04:35 +0800

    feat(2020-18/angch): Fancy goroutines

--- 2020-18/angch/main.go
+++ 2020-18/angch/main.go
@@ -201,44 +201,45 @@ func do2(fileName string) (ret1 int, ret2 int) {
 		l = strings.ReplaceAll(l, ")", " ) ")
 		l = strings.ReplaceAll(l, "  ", " ")
 		toks := strings.Split(l, " ")
-		num := 0
 
-		tokbuffer := make([]string, 0)
-		opstack := make([]string, 0)
-		op := ""
+		tokbuffer := make(chan string)
 		log.Println("Input:", toks)
-		for _, v := range toks {
-			if v == "" {
-				continue
-			}
-			if v == "(" {
-				opstack = append(opstack, v)
-				log.Println("RPN:", v, tokbuffer, opstack)
-				continue
-			}
-			if v == ")" {
-				for len(opstack) > 0 {
-					top := opstack[len(opstack)-1]
-					if top != "(" {
-						tokbuffer = append(tokbuffer, top)
-						opstack = opstack[:len(opstack)-1]
-					} else if top == "(" {
-						opstack = opstack[:len(opstack)-1]
-						break
-					}
+		log.Println("Goroutine #1")
+		go func() {
+			opstack := make([]string, 0)
+			for _, v := range toks {
+				if v == "" {
+					continue
 				}
-				log.Println("RPN:", v, tokbuffer, opstack)
-				continue
-			}
-			if v == "+" || v == "-" || v == "*" {
-				if len(tokbuffer) == 0 || tokbuffer[len(tokbuffer)-1] == "(" {
-					tokbuffer = append(tokbuffer, v)
-				} else {
+				if v == "(" {
+					opstack = append(opstack, v)
+					log.Println("RPN:", v, opstack)
+					continue
+				}
+				if v == ")" {
+					for len(opstack) > 0 {
+						top := opstack[len(opstack)-1]
+						if top != "(" {
+							// tokbuffer = append(tokbuffer, top)
+							log.Println("RPN: push", top)
+							tokbuffer <- top
+							opstack = opstack[:len(opstack)-1]
+						} else if top == "(" {
+							opstack = opstack[:len(opstack)-1]
+							break
+						}
+					}
+					log.Println("RPN:", v, opstack)
+					continue
+				}
+				if v == "+" || v == "-" || v == "*" {
 					for len(opstack) > 0 {
 						top := opstack[len(opstack)-1]
 
 						if (top == "-" || top == "+") && top != "(" {
-							tokbuffer = append(tokbuffer, top)
+							// tokbuffer = append(tokbuffer, top)
+							log.Println("RPN: push", top)
+							tokbuffer <- top
 							opstack = opstack[:len(opstack)-1]
 							continue
 						}
@@ -246,29 +247,30 @@ func do2(fileName string) (ret1 int, ret2 int) {
 						break
 					}
 					opstack = append(opstack, v)
+					// }
+					log.Println("RPN:", v, opstack)
+					continue
 				}
-				log.Println("RPN:", v, tokbuffer, opstack)
-				continue
+
+				// tokbuffer = append(tokbuffer, v)
+				log.Println("RPN: push", v)
+				tokbuffer <- v
+				// log.Println("RPN:", v, tokbuffer, opstack)
 			}
 
-			tokbuffer = append(tokbuffer, v)
-			if op != "" {
-				tokbuffer = append(tokbuffer, op)
+			for len(opstack) > 0 {
+				top := opstack[len(opstack)-1]
+				opstack = opstack[:len(opstack)-1]
+				tokbuffer <- top
 			}
-			log.Println("RPN:", v, tokbuffer, opstack)
-		}
-
-		for len(opstack) > 0 {
-			top := opstack[len(opstack)-1]
-			opstack = opstack[:len(opstack)-1]
-			tokbuffer = append(tokbuffer, top)
-		}
-
-		log.Println("RPN:", tokbuffer, opstack)
+			log.Println("RPN: Close channel", opstack)
+			close(tokbuffer)
+		}()
 
 		stack := make([]int, 0)
-		for _, v := range tokbuffer {
-			// log.Println("eval:", v, stack)
+		log.Println("\t\t\t\tGoroutine #2")
+		for v := range tokbuffer {
+			log.Println("\t\t\t\teval:", v, stack)
 			if v == "*" || v == "+" || v == "-" {
 				n1 := stack[len(stack)-1]
 				n2 := stack[len(stack)-2]
@@ -283,7 +285,7 @@ func do2(fileName string) (ret1 int, ret2 int) {
 					n3 := n1 * n2
 					stack = append(stack, n3)
 				}
-				log.Println("eval:", v, stack)
+				log.Println("\t\t\t\teval:", v, stack)
 				continue
 			}
 			num, err := strconv.Atoi(v)
@@ -291,9 +293,9 @@ func do2(fileName string) (ret1 int, ret2 int) {
 				log.Fatal(err)
 			}
 			stack = append(stack, num)
-			log.Println("eval:", v, stack)
+			// log.Println("eval:", v, stack)
 		}
-		num = stack[0]
+		num := stack[0]
 
 		log.Println("Result:", num)
 		sum += num
