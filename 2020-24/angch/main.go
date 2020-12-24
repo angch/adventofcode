4da75ed025f431636da676b53187ca4946169698
Ang Chin Han <angch@tuxuri.com>
2020-12-24 14:57:48 +0800

    feat(2020-24/angch): More neighbourhoodcount optimization

--- 2020-24/angch/main.go
+++ 2020-24/angch/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Hex struct {
-	I, J, K int
+	I, J, K int16
+	pad     int16
 }
 
-func Cube(i, j, k int) Hex {
-	return Hex{i, j, k}
+func Cube(i, j, k int16) Hex {
+	return Hex{i, j, k, 0}
 }
 
 var dir = map[string]Hex{
@@ -64,12 +65,12 @@ func do(fileName string) (ret1 int, ret2 int) {
 	for c, v2 := range hexmap {
 		if v2 {
 			ret1++
+			// neighbourcount[c] += 0
 
 			for _, v := range dir {
 				v.I += c.I
 				v.J += c.J
 				v.K += c.K
-
 				neighbourcount[v]++
 			}
 		}
@@ -78,107 +79,42 @@ func do(fileName string) (ret1 int, ret2 int) {
 	for day := 1; day <= 100; day++ {
 		hexmap2 := make(map[Hex]bool)
 		neighbourcount2 := make(map[Hex]int)
-		min, max := minmax(hexmap)
-		for i := min.I - 1; i <= max.I+1; i++ {
-			for j := min.J - 1; j <= max.J+1; j++ {
-				for k := min.K - 1; k <= max.K+1; k++ {
-					c := Hex{i, j, k}
-					n := neighbourcount[c]
-					// n := neighbour(hexmap, c)
-					if hexmap[c] {
-						if n == 0 || n > 2 {
-							// hexmap2[c] = false
-						} else {
-							hexmap2[c] = true
-							for _, v := range dir {
-								v.I += c.I
-								v.J += c.J
-								v.K += c.K
-								neighbourcount2[v]++
-							}
-						}
-					} else {
-						if n == 2 {
-							hexmap2[c] = true
-							for _, v := range dir {
-								v.I += c.I
-								v.J += c.J
-								v.K += c.K
-								neighbourcount2[v]++
-							}
-						} else {
-							// hexmap2[c] = false
-						}
+		for c, n := range neighbourcount {
+			if hexmap[c] {
+				if !(n == 0 || n > 2) {
+					hexmap2[c] = true
+					for _, v := range dir {
+						v.I += c.I
+						v.J += c.J
+						v.K += c.K
+						neighbourcount2[v]++
+						// neighbourcount2[c] += 0
+					}
+				}
+			} else {
+				if n == 2 {
+					hexmap2[c] = true
+					for _, v := range dir {
+						v.I += c.I
+						v.J += c.J
+						v.K += c.K
+						neighbourcount2[v]++
+						// neighbourcount2[c] += 0
 					}
 				}
 			}
 		}
 		hexmap = hexmap2
-
-		count := 0
 		neighbourcount = neighbourcount2
-		for _, v2 := range hexmap {
-			if v2 {
-				count++
-			}
-		}
-		ret2 = count
-		// log.Println("Day", day, count)
+
+		ret2 = len(hexmap)
+		log.Println("Day", day, ret2, len(neighbourcount2))
 	}
 
 	return ret1, ret2
 }
 
-func minmax(hexmap map[Hex]bool) (Hex, Hex) {
-	min := Hex{}
-	max := Hex{}
-	first := true
-	for k, v := range hexmap {
-		if !v {
-			continue
-		}
-		if first {
-			first = false
-			min = k
-			max = k
-			continue
-		}
-		if min.I > k.I {
-			min.I = k.I
-		}
-		if min.J > k.J {
-			min.J = k.J
-		}
-		if min.K > k.K {
-			min.K = k.K
-		}
-		if max.I < k.I {
-			max.I = k.I
-		}
-		if max.J < k.J {
-			max.J = k.J
-		}
-		if max.K < k.K {
-			max.K = k.K
-		}
-	}
-	return min, max
-}
-
-func neighbour(hexmap map[Hex]bool, c Hex) int {
-	count := 0
-	for _, v := range dir {
-		v.I += c.I
-		v.J += c.J
-		v.K += c.K
-
-		if hexmap[v] {
-			count++
-		}
-	}
-	return count
-}
 func main() {
-	log.Println(do("test.txt"))
+	// log.Println(do("test.txt"))
 	log.Println(do("input.txt"))
 }
