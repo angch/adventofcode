98412c15168156fad6455d5d64b1a4f53d972a97
Ang Chin Han <angch@tuxuri.com>
2021-02-01 16:59:42 +0800

    refac(2020-21): Cleanup

--- 2020-21/angch/main.go
+++ 2020-21/angch/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"sort"
 	"strings"
-	"time"
-
-	"github.com/gnboorse/centipede"
 )
 
 type Rule struct {
@@ -18,7 +14,7 @@ type Rule struct {
 }
 
 func intersect(a, b []string) []string {
-	log.Println("intersect", a, b)
+	// log.Println("intersect", a, b)
 	a_ := make(map[string]bool)
 	c := make([]string, 0)
 	for _, v := range a {
@@ -42,7 +38,7 @@ func inslice(a string, b []string) bool {
 	return false
 }
 
-func do(fileName string) (ret1 int, ret2 int) {
+func do(fileName string) (ret1 int, ret2 string) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -77,7 +73,7 @@ func do(fileName string) (ret1 int, ret2 int) {
 
 		ingredients := strings.Split(strings.TrimSpace(rule[0]), " ")
 
-		log.Println(ingredients, contains)
+		// log.Println(ingredients, contains)
 
 		for _, v := range ingredients {
 			e, ok := possibleAllergen[v]
@@ -125,7 +121,8 @@ func do(fileName string) (ret1 int, ret2 int) {
 		allergen := make(map[string][]string)
 		counts := make(map[string]int)
 		for k, v := range rules {
-			log.Println(k, v)
+			// log.Println(k, v)
+			_ = k
 			for _, all := range v.Allergen {
 				if _, ok := allergen[all]; !ok {
 					allergen[all] = make([]string, len(v.Ingredient))
@@ -149,7 +146,7 @@ func do(fileName string) (ret1 int, ret2 int) {
 			}
 			ret1 += v
 		}
-		log.Println("out", allergen, ret1)
+		// log.Println("out", allergen, ret1)
 		confirmed := make(map[string]string)
 	b:
 		for k, v := range allergen {
@@ -180,133 +177,13 @@ func do(fileName string) (ret1 int, ret2 int) {
 		for _, v := range keys {
 			outdanger = append(outdanger, confirmed[v])
 		}
-		log.Println("out2", confirmed, strings.Join(outdanger, ","))
+		// log.Println("out2", confirmed, strings.Join(outdanger, ","))
+		ret2 = strings.Join(outdanger, ",")
 	}
 
-	if false {
-		doesnotContain := make(map[string]map[string]bool)
-		for rulen, rule := range rules {
-			al := make(map[string]bool)
-			notal := make([]string, 0)
-			for _, al2 := range rule.Allergen {
-				al[al2] = true
-			}
-			for al2 := range allergenInIngredient {
-				if !al[al2] {
-					notal = append(notal, al2)
-				}
-			}
-
-			for _, ingredient := range rule.Ingredient {
-				log.Println("Rule #", rulen+1, ingredient, "does not contain", notal)
-
-				c, ok := doesnotContain[ingredient]
-				if !ok {
-					doesnotContain[ingredient] = make(map[string]bool)
-					c = doesnotContain[ingredient]
-				}
-				for _, al2 := range notal {
-					c[al2] = true
-				}
-			}
-		}
-		log.Println(doesnotContain)
-	}
-	log.Println("allergenid", allergenId)
-
-	if false {
-		// Ah fuggit.
-		vars := make(centipede.Variables, 0)
-		for i := 0; i < len(ingredient); i++ {
-			varName := centipede.VariableName(ingredient[i])
-			cvar := centipede.NewVariable(varName, centipede.IntRange(1, len(allergen)+1))
-			vars = append(vars, cvar)
-		}
-
-		constraints := make([]centipede.Constraint, 0)
-		allergenIds := make([][]int, 0)
-		for ruleN, rule := range rules {
-			varnames := make([]centipede.VariableName, 0)
-			vv := make(map[string]bool)
-			for _, id := range rule.Ingredient {
-				varnames = append(varnames, centipede.VariableName(id))
-				vv[id] = true
-			}
-
-			allergenIds = append(allergenIds, make([]int, 0))
-			for _, id := range rule.Allergen {
-				allergenIds[ruleN] = append(allergenIds[ruleN], allergenId[id])
-			}
-
-			ConstraintFunction := foo(ruleN, allergenIds, vv)
-			log.Println("rule c added", ruleN, allergenIds, varnames)
-
-			constraints = append(constraints, centipede.Constraint{Vars: varnames, ConstraintFunction: ConstraintFunction})
-		}
-		log.Println(vars, constraints)
-		//constraints = append(constraints, centipede.Constraint{Vars: varnames, ConstraintFunction: ConstraintFunction})
-
-		solver := centipede.NewBackTrackingCSPSolver(vars, constraints)
-		begin := time.Now()
-		success := solver.Solve() // run the solution
-		elapsed := time.Since(begin)
-		if success {
-			for _, variable := range solver.State.Vars {
-				fmt.Printf("Variable %v = %v\n", variable.Name, variable.Value)
-			}
-		} else {
-			log.Println("not found")
-		}
-		log.Println(elapsed)
-
-		// 	centipede.NewVariable("A", centipede.IntRange(1, 10)),
-		// 	centipede.NewVariable("B", centipede.IntRange(1, 10)),
-		// 	centipede.NewVariable("C", centipede.IntRange(1, 10)),
-		// 	centipede.NewVariable("D", centipede.IntRange(1, 10)),
-		// 	centipede.NewVariable("E", centipede.IntRangeStep(0, 20, 2)), // even numbers < 20
-		// }
-	}
-	// count := 0
-	// for k, v := range doesnotContain {
-	// 	if len(v) == len(allergenInIngredient) {
-	// 		log.Println(k, "is ok", appearances[k])
-	// 		count += appearances[k]
-	// 	}
-	// }
-	// ret1 = count
+	// log.Println("allergenid", allergenId)
 
 	return ret1, ret2
-}
-
-func foo(n int, allergenIds [][]int, vv map[string]bool) func(variables *centipede.Variables) bool {
-	return func(variables *centipede.Variables) bool {
-		found := 0
-		nn := make([]string, 0)
-		for _, v := range *variables {
-			nn = append(nn, string(v.Name))
-		}
-		log.Println("Rule ", n, nn)
-
-		for _, id := range allergenIds[n] {
-			for _, v := range *variables {
-				if !vv[string(v.Name)] {
-					continue
-				}
-				if !v.Empty {
-					log.Println("xx rule", n, v.Name, v.Value, id+1)
-					i, ok := v.Value.(int)
-					if ok && i == id+1 {
-						found++
-						// log.Println("found")
-						break
-					}
-				}
-			}
-		}
-		log.Println("found", found, len(allergenIds[n]), found == len(allergenIds[n]))
-		// return found > 0
-		return found == len(allergenIds[n])
-	}
 }
 
 func main() {
