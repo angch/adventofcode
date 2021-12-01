61be0d9447e555eeca9d9123d10ca0aa35e137ae
Ang Chin Han <angch@tuxuri.com>
2021-12-01 14:42:35 +0800

    refac(angch/2021-01): clean ups

--- angch/2021-01/main.go
+++ angch/2021-01/main.go
@@ -7,25 +7,27 @@ import (
 	"strconv"
 )
 
+const p1, p2 = 2, 4
+
 func main() {
 	file, _ := os.Open("test.txt")
 	scanner := bufio.NewScanner(file)
 	var part1, part2 int
-	win := make([]int, 0)
+	var win []int
 	for scanner.Scan() {
 		line := scanner.Text()
 		n, _ := strconv.Atoi(line)
 		win = append(win, n)
-		if len(win) >= 2 {
-			if n > win[len(win)-2] {
+		if len(win) >= p1 {
+			if n > win[len(win)-p1] {
 				part1++
 			}
 		}
-		if len(win) >= 4 {
-			a := win[len(win)-4]
-			if n > a {
+		if len(win) >= p2 {
+			if n > win[len(win)-p2] {
 				part2++
 			}
+			win = win[1:]
 		}
 	}
 	fmt.Println(part1, part2)
