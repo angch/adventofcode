6f7c5828840466728cb588d89f289dde3b520b60
Ang Chin Han <angch@tuxuri.com>
2021-12-03 15:14:19 +0800

    refac(2021-03/angch): cleanup

--- angch/2021-03/main.go
+++ angch/2021-03/main.go
@@ -6,23 +6,32 @@ import (
 	"os"
 )
 
+var filepath = "input.txt"
+
+// var filepath = "test.txt"
+var debug = false
+
+func str2bin(s string) int {
+	bin := 0
+	for _, v := range s {
+		bin <<= 1
+		if v == '1' {
+			bin |= 1
+		}
+	}
+	return bin
+}
+
 func part1() {
-	// file, _ := os.Open("test.txt")
-	file, _ := os.Open("input.txt")
+	file, _ := os.Open(filepath)
+	// file, _ := os.Open("input.txt")
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	lines := []string{}
-	oxygen := []string{}
-	co2 := []string{}
 	counts := make([][2]int, 26)
 	length := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		_ = line
-		lines = append(lines, line)
-		oxygen = append(oxygen, line)
-		co2 = append(co2, line)
-
 		for k, v := range line {
 			if v == '0' {
 				counts[k][0]++
@@ -35,7 +44,6 @@ func part1() {
 	gamma := 0
 	epsilon := 0
 	for i := 0; i < length; i++ {
-		// fmt.Println("pos ", i, " ", counts[i])
 		gamma <<= 1
 		epsilon <<= 1
 		if counts[i][0] < counts[i][1] {
@@ -43,12 +51,28 @@ func part1() {
 			// fmt.Print("1")
 		} else {
 			epsilon |= 1
-			// fmt.Print("0")
 		}
 	}
 	fmt.Println(gamma * epsilon)
 
+}
+
+func part2() {
+	file, _ := os.Open(filepath)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	oxygen := []string{}
+	co2 := []string{}
 	sum1, sum2 := 0, 0
+
+	length := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		oxygen = append(oxygen, line)
+		co2 = append(co2, line)
+		length = len(line)
+	}
 
 	for i := 0; i < length; i++ {
 		counts := [2]int{0, 0}
@@ -71,17 +95,15 @@ func part1() {
 		}
 		oxygen = oxygen2
 		if len(oxygen) == 1 {
-			fmt.Println("oxy", oxygen[0])
-			for _, v := range oxygen[0] {
-				sum1 <<= 1
-				if v == '1' {
-					sum1 |= 1
-				}
+			if debug {
+				fmt.Println("oxy", oxygen[0])
 			}
+			sum1 = str2bin(oxygen[0])
 			break
 		}
-		fmt.Println(i+1, oxygen, mostcommon)
-
+		if debug {
+			fmt.Println(i+1, oxygen, mostcommon)
+		}
 	}
 	for i := 0; i < length; i++ {
 		counts := [2]int{0, 0}
@@ -92,44 +114,29 @@ func part1() {
 				counts[1]++
 			}
 		}
-		mostcommon := '0'
+		leastcommon := '0'
 		if counts[0] > counts[1] {
-			mostcommon = '1'
+			leastcommon = '1'
 		}
 		co22 := make([]string, 0)
 		for _, v := range co2 {
-			if v[i] == byte(mostcommon) {
+			if v[i] == byte(leastcommon) {
 				co22 = append(co22, v)
 			}
 		}
 		co2 = co22
 		if len(co2) == 1 {
-			fmt.Println("co2", co2[0])
-			for _, v := range co2[0] {
-				sum2 <<= 1
-				if v == '1' {
-					sum2 |= 1
-				}
+			if debug {
+				fmt.Println("co2", co2[0])
 			}
+			sum2 = str2bin(co2[0])
 			break
 		}
 	}
-	fmt.Println(sum1, sum2, sum1*sum2)
+	fmt.Println(sum1 * sum2)
 }
 
-func part2() {
-	file, _ := os.Open("test.txt")
-	// file, _ := os.Open("input.txt")
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		_ = line
-	}
-
-}
 func main() {
 	part1()
-	// part2()
+	part2()
 }
