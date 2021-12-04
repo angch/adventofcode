94a365a22d3a39afd77c9f5b937e9ff38549d46c
Ang Chin Han <ang.chin.han@gmail.com>
2021-12-04 21:28:32 +0800

    feat(2021-04/angch): Faster algo, mark, sum and check bingo in one op.

---
+++ angch/2021-04/2021-04-refac/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var filepath = "input.txt"
+
+// var filepath = "test.txt"
+
+type board struct {
+	nums   map[int][2]int
+	sum    int
+	count1 []int
+	count2 []int
+	bingo  bool
+}
+
+func boardFromLines(lines []string) *board {
+	n := len(lines)
+	b := board{
+		nums:   make(map[int][2]int),
+		count1: make([]int, n),
+		count2: make([]int, n),
+	}
+	sum := 0
+
+	for i, line := range lines {
+		ns := strings.Split(line, " ")
+		j := 0
+		for _, n := range ns {
+			if n != "" {
+				num := 0
+				fmt.Sscanf(n, "%d", &num)
+				b.nums[num] = [2]int{i, j}
+				j++
+				sum += num
+			}
+		}
+		b.count1[i] = n
+		b.count2[i] = n
+	}
+	b.sum = sum
+	return &b
+}
+
+func (b *board) mark(n int) {
+	coord, ok := b.nums[n]
+	if ok {
+		b.count1[coord[0]]--
+		b.count2[coord[1]]--
+		b.sum -= n
+		if b.count1[coord[0]] == 0 || b.count2[coord[1]] == 0 {
+			b.bingo = true
+			return
+		}
+	}
+}
+
+func day4() {
+	file, _ := os.Open(filepath)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	nums := scanner.Text()
+	boards := make([]*board, 0)
+	scanner.Scan()
+
+	for {
+		lines := make([]string, 0)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if line == "" {
+				break
+			}
+			lines = append(lines, line)
+		}
+		if len(lines) == 0 {
+			break
+		}
+		boards = append(boards, boardFromLines(lines))
+	}
+	drawnNums := strings.Split(nums, ",")
+
+	won := 0
+	for _, v := range drawnNums {
+		b, _ := strconv.Atoi(v)
+
+		for _, board := range boards {
+			if board.bingo {
+				continue
+			}
+			board.mark(b)
+
+			if board.bingo {
+				won++
+				if won == 1 {
+					fmt.Println("Part 1 Score:", board.sum*b)
+				} else if won == len(boards) {
+					fmt.Println("Part 2 Score:", board.sum*b)
+					return
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	day4()
+}
