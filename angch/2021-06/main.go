275e4f2c9747a015f74285c195c8893a1d9900de
Ang Chin Han <angch@tuxuri.com>
2021-12-06 13:35:33 +0800

    feat(2021-06/angch): Optimized

--- angch/2021-06/main.go
+++ angch/2021-06/main.go
@@ -19,59 +19,30 @@ func part1and2() {
 	file, _ := os.Open(filepath)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	// lines := make([]Line, 0)
-
 	scanner.Scan()
-	t := scanner.Text()
-	ages := make([]int, 0)
-	agesStr := strings.Split(t, ",")
+	agesStr := strings.Split(scanner.Text(), ",")
 
+	ageCounts := [9]int{}
+	total := len(agesStr)
 	for _, age := range agesStr {
 		a, _ := strconv.Atoi(age)
-		ages = append(ages, a)
+		ageCounts[a]++
 	}
 
-	if false {
-		// Yeah, don't do this
-		for day := 0; day < 80; {
-			add := 0
-			for i := 0; i < len(ages); i++ {
-				ages[i]--
-				if ages[i] < 0 {
-					ages[i] = 6
-					add++
-				}
-			}
-			for ; add > 0; add-- {
-				ages = append(ages, 8)
-			}
-			day++
-		}
-	}
-
-	ageCounts := make([]int, 9)
-	for _, age := range ages {
-		ageCounts[age]++
-	}
-
+	index := 0
 	for day := 0; day < 256; {
-		zero := ageCounts[0]
-		ageCounts = ageCounts[1:]
-		ageCounts[6] += zero
-		ageCounts = append(ageCounts, zero)
+		zero := ageCounts[index]
+		index++
+		if index >= 9 {
+			index = 0
+		}
+		ageCounts[(6+index)%9] += zero
 		day++
+		total += zero
 
 		if day == 80 {
-			total := 0
-			for i := 0; i < len(ageCounts); i++ {
-				total += ageCounts[i]
-			}
 			fmt.Println("Part 1", total)
 		}
-	}
-	total := 0
-	for i := 0; i < len(ageCounts); i++ {
-		total += ageCounts[i]
 	}
 
 	fmt.Println("Part 2", total)
