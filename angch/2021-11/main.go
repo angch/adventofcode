e22578680df4f783f1f4f34678cc1c57b06e5668
Ang Chin Han <ang.chin.han@gmail.com>
2021-12-11 13:22:33 +0800

    feat(2021-11/angch): Init

---
+++ angch/2021-11/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Coord struct{ X, Y int }
+
+var adj = []Coord{
+	{-1, 0}, {1, 0}, {0, -1}, {0, 1},
+	{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
+}
+
+func flood(board map[Coord]int, c Coord) int {
+	basin := make(map[Coord]bool)
+	basin[c] = true
+	eval := []Coord{c}
+
+	for len(eval) > 0 {
+		var c1 Coord
+		c1, eval = eval[len(eval)-1], eval[:len(eval)-1]
+		for _, d := range adj {
+			c2 := Coord{c1.X + d.X, c1.Y + d.Y}
+			v2, ok := board[c2]
+			if !ok {
+				continue
+			}
+			v1 := board[c1]
+			if v2 > v1 && v2 < 9 {
+				if !basin[c2] {
+					basin[c2] = true
+					eval = append(eval, c2)
+				}
+			}
+		}
+	}
+	return len(basin)
+}
+
+func day11(filepath string) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	board := make(map[Coord]int)
+	c := Coord{0, 0}
+	for scanner.Scan() {
+		t := scanner.Text()
+		for _, v := range t {
+			board[c] = int(v - '0')
+			c.X++
+		}
+		c = Coord{0, c.Y + 1}
+	}
+	// fmt.Println(board)
+
+	// areas := make([]int, 0)
+	part1 := 0
+	part2 := 0
+
+	// next:
+
+	for step := 1; ; step++ {
+		// board2 := board
+		// fmt.Println("Step", step)
+
+		flashed := make(map[Coord]bool)
+		for c, _ := range board {
+			board[c]++
+		}
+		again := true
+
+		for again {
+			again = false
+
+			for c, v := range board {
+				if v > 9 {
+					flashed[c] = true
+					board[c] = 0
+					if step <= 100 {
+						part1++
+					}
+					for _, d := range adj {
+						c2 := Coord{c.X + d.X, c.Y + d.Y}
+						_, ok := board[c2]
+						if !ok {
+							continue
+						}
+						if !flashed[c2] {
+							board[c2]++
+							again = true
+						}
+					}
+				}
+			}
+		}
+		if len(flashed) == len(board) {
+			part2 = step
+			break
+		}
+		// fmt.Println("After step", step, part1)
+		// for y := 0; y < 10; y++ {
+		// 	for x := 0; x < 10; x++ {
+		// 		c := Coord{x, y}
+		// 		v, _ := board[c]
+		// 		fmt.Print(v)
+		// 	}
+		// 	fmt.Println()
+		// }
+	}
+
+	fmt.Println("Part 1", part1)
+	// sort.Ints(areas)
+	// part2 := 1
+	// for k := 0; k < 3; k++ {
+	// 	part2 *= areas[len(areas)-1-k]
+	// }
+	fmt.Println("Part 2", part2)
+}
+
+func main() {
+	day11("test.txt")
+	day11("input.txt")
+}
