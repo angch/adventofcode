f4f7cd386e58bfedb15b01643ea018bbd4dd9731
Ang Chin Han <ang.chin.han@gmail.com>
2021-12-11 13:24:28 +0800

    fix: Remove old code

--- angch/2021-11/main.go
+++ angch/2021-11/main.go
@@ -13,32 +13,6 @@ var adj = []Coord{
 	{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
 }
 
-func flood(board map[Coord]int, c Coord) int {
-	basin := make(map[Coord]bool)
-	basin[c] = true
-	eval := []Coord{c}
-
-	for len(eval) > 0 {
-		var c1 Coord
-		c1, eval = eval[len(eval)-1], eval[:len(eval)-1]
-		for _, d := range adj {
-			c2 := Coord{c1.X + d.X, c1.Y + d.Y}
-			v2, ok := board[c2]
-			if !ok {
-				continue
-			}
-			v1 := board[c1]
-			if v2 > v1 && v2 < 9 {
-				if !basin[c2] {
-					basin[c2] = true
-					eval = append(eval, c2)
-				}
-			}
-		}
-	}
-	return len(basin)
-}
-
 func day11(filepath string) {
 	file, err := os.Open(filepath)
 	if err != nil {
