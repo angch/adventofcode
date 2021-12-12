7cb1b8dba5c77532f56dba242e13fcedfc781d68
Ang Chin Han <ang.chin.han@gmail.com>
2021-12-13 01:14:08 +0800

    refac(2021-12/angch): Micro optimizations

--- angch/2021-12/main.go
+++ angch/2021-12/main.go
@@ -16,44 +16,73 @@ func day12(filepath string) {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	edge := make(map[string][]string)
+	edge := make(map[int][]int)
+	node := make(map[string]int)
+	nodeid := 3
+	node["start"] = 1
+	node["end"] = 2
+	isSmall := make(map[int]bool)
+	isSmall[1] = true
+	isSmall[2] = true
 	for scanner.Scan() {
 		a := strings.Split(scanner.Text(), "-")
-		from, to := a[0], a[1]
-		_, ok := edge[from]
+		// from, to := a[0], a[1]
+		_, ok := node[a[0]]
 		if !ok {
-			edge[from] = make([]string, 0, 1)
+			node[a[0]] = nodeid
+			if a[0][0] >= 'a' && a[0][0] <= 'z' {
+				isSmall[nodeid] = true
+			} else {
+				isSmall[nodeid] = false
+			}
+			nodeid++
+		}
+		_, ok = node[a[1]]
+		if !ok {
+			node[a[1]] = nodeid
+			if a[1][0] >= 'a' && a[1][0] <= 'z' {
+				isSmall[nodeid] = true
+			} else {
+				isSmall[nodeid] = false
+			}
+			nodeid++
+		}
+
+		from, to := node[a[0]], node[a[1]]
+		_, ok = edge[from]
+		if !ok {
+			edge[from] = make([]int, 0, 1)
 		}
 		_, ok = edge[to]
 		if !ok {
-			edge[to] = make([]string, 0, 1)
+			edge[to] = make([]int, 0, 1)
 		}
 		edge[to] = append(edge[to], from)
 		edge[from] = append(edge[from], to)
 	}
 
 	part1, part2 := 0, 0
-	eval := [][]string{{"end"}}
+	eval := [][]int{{node["end"]}}
 
 	for len(eval) > 0 {
-		var path []string
+		var path []int
 		path, eval = eval[len(eval)-1], eval[:len(eval)-1]
-		slicecnt := make(map[string]int)
+		slicecnt := make([]int, nodeid)
 		hasSmall := false
 		for _, v := range path {
 			slicecnt[v]++
 			if slicecnt[v] > 1 {
-				if v[0] >= 'a' && v[0] <= 'z' {
+				if isSmall[v] {
 					hasSmall = true
 				}
 			}
 		}
 
-		end := path[0]
+		end := path[len(path)-1]
 		for _, k := range edge[end] {
 			c := slicecnt[k]
-			if k != "start" && k != "end" {
-				if k[0] >= 'a' && k[0] <= 'z' {
+			if k != 1 && k != 2 {
+				if isSmall[k] {
 					// Small cave
 					if c > 0 && hasSmall {
 						continue
@@ -62,15 +91,17 @@ func day12(filepath string) {
 			} else if c > 0 {
 				continue
 			}
-			path2 := append([]string{k}, path...)
 
-			if k == "start" {
+			if k == 1 {
 				if !hasSmall {
 					part1++
 				}
 				part2++
 				continue
 			}
+			path2 := make([]int, len(path), len(path)+1)
+			copy(path2, path)
+			path2 = append(path2, k)
 			eval = append(eval, path2)
 		}
 	}
