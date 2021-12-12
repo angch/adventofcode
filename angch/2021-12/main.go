385205221989b78174e78c208b0a84e32aa70187
Ang Chin Han <ang.chin.han@gmail.com>
2021-12-12 20:43:48 +0800

    refac(2021-12/angch): Faster

--- angch/2021-12/main.go
+++ angch/2021-12/main.go
@@ -3,169 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
+	"time"
 )
-
-type Coord struct{ X, Y int }
-
-type Edge struct {
-	From, To string
-}
-
-// func visit0(nodeEdge map[string]map[string]bool, visited map[string]int, start, end string) [][]string {
-// 	log.Println("visit", start, end)
-// 	if start == end {
-// 		log.Println(start, "-", end, "returning nil")
-// 		return nil
-// 	}
-
-// 	paths := make([][]string, 0)
-
-// 	for k, _ := range nodeEdge[start] {
-// 		visited[k]++
-// 		log.Println("k is", k)
-
-// 		if k == end {
-// 			// log.Println(start, end, "subpaths is", subpaths)
-// 			path := []string{start, k}
-// 			paths = append(paths, path)
-// 			log.Println(start, end, "paths is", paths)
-// 		} else {
-// 			subpaths := visit0(nodeEdge, visited, k, end)
-// 			if len(subpaths) > 0 {
-// 				log.Println("paths is", paths)
-// 				for _, p := range subpaths {
-// 					path := []string{start}
-// 					path = append(path, p...)
-// 					// path = append(path, end)
-// 					paths = append(paths, path)
-// 					log.Println("path is", path)
-// 				}
-// 			}
-// 		}
-// 	}
-// 	log.Println(start, "-", end, "returning", paths)
-
-// 	return paths
-// }
-
-func slicecount(s []string, e string) int {
-	c := 0
-	for _, a := range s {
-		if a == e {
-			c++
-		}
-	}
-	return c
-}
-
-func visit1(nodeEdge map[string]map[string]bool, start, end string) [][]string {
-	// log.Println("visit2", start, end, paths)
-
-	eval := make([][]string, 0)
-	eval = [][]string{[]string{end}}
-	out := make([][]string, 0)
-
-	for len(eval) > 0 {
-		path := eval[0]
-		eval = eval[1:]
-
-		end := path[0]
-		for k, _ := range nodeEdge[end] {
-			c := slicecount(path, k)
-			if k != "start" && k != "end" {
-				if strings.ToUpper(k) != k {
-					// Small cave
-					if c > 0 {
-						// fmt.Println(" skip", k, path, c)
-						continue
-					}
-
-				} else {
-					// if c > 20 {
-					// 	continue
-					// }
-				}
-			} else {
-				if c > 0 {
-					continue
-				}
-			}
-			path2 := []string{k}
-			path2 = append(path2, path...)
-
-			if k == start {
-				out = append(out, path2)
-			}
-			eval = append(eval, path2)
-		}
-	}
-
-	return out
-}
-
-func visit2(nodeEdge map[string]map[string]bool, start, end string) [][]string {
-	// log.Println("visit2", start, end, paths)
-
-	eval := make([][]string, 0)
-	eval = [][]string{[]string{end}}
-	out := make([][]string, 0)
-
-	for len(eval) > 0 {
-		path := eval[0]
-		eval = eval[1:]
-
-		end := path[0]
-		for k, _ := range nodeEdge[end] {
-			c := slicecount(path, k)
-			if k != "start" && k != "end" {
-				if strings.ToUpper(k) != k {
-					// Small cave
-					if c > 0 {
-						// fmt.Println(" skip", k, path, c)
-						// hack hack
-						smallcount := make(map[string]int)
-						hasSmall := false
-						for _, p := range path {
-							if strings.ToUpper(p) != p {
-								smallcount[p]++
-								if smallcount[p] > 1 {
-									hasSmall = true
-									break
-								}
-							}
-						}
-						if hasSmall {
-							continue
-						}
-						// continue
-					}
-
-				} else {
-					// if c > 20 {
-					// 	continue
-					// }
-				}
-			} else {
-				if c > 0 {
-					continue
-				}
-			}
-			path2 := []string{k}
-			path2 = append(path2, path...)
-
-			if k == start {
-				out = append(out, path2)
-				continue
-			}
-			eval = append(eval, path2)
-		}
-	}
-
-	return out
-}
 
 func day12(filepath string) {
 	file, err := os.Open(filepath)
@@ -175,70 +16,74 @@ func day12(filepath string) {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	edges := make(map[Edge]bool)
-	nodeEdge := make(map[string]map[string]bool)
-	nodeEdge2 := make(map[string]map[string]bool)
-
+	edge := make(map[string][]string)
 	for scanner.Scan() {
-		t := scanner.Text()
-		a := strings.Split(t, "-")
-		e := Edge{a[0], a[1]}
-		edges[e] = true
-		_, ok := nodeEdge[a[0]]
+		a := strings.Split(scanner.Text(), "-")
+		from, to := a[0], a[1]
+		_, ok := edge[from]
 		if !ok {
-			nodeEdge[a[0]] = make(map[string]bool)
+			edge[from] = make([]string, 0, 1)
 		}
-		_, ok = nodeEdge[a[1]]
+		_, ok = edge[to]
 		if !ok {
-			nodeEdge[a[1]] = make(map[string]bool)
+			edge[to] = make([]string, 0, 1)
 		}
-		_, ok = nodeEdge2[a[1]]
-		if !ok {
-			nodeEdge2[a[1]] = make(map[string]bool)
-		}
-		_, ok = nodeEdge2[a[0]]
-		if !ok {
-			nodeEdge2[a[0]] = make(map[string]bool)
-		}
-
-		nodeEdge[a[0]][a[1]] = true
-
-		_, ok = nodeEdge2[a[1]]
-		if !ok {
-			nodeEdge2[a[1]] = make(map[string]bool)
-		}
-		nodeEdge2[a[1]][a[0]] = true
-		if a[1] != "end" {
-			nodeEdge[a[0]][a[1]] = true
-		}
-
-		nodeEdge2[a[0]][a[1]] = true
+		edge[to] = append(edge[to], from)
+		edge[from] = append(edge[from], to)
 	}
-	// fmt.Println(edges)
-	// fmt.Println(nodeEdge)
-	// visited := make(map[string]int)
-	// a := visit(nodeEdge, visited, "start", "end")
-	visited := make(map[string]bool)
-	visited["end"] = true
-	part1 := make(chan int)
-	part2 := make(chan int)
-	go func() {
-		a := visit1(nodeEdge2, "start", "end")
-		part1 <- len(a)
-	}()
-	go func() {
-		a := visit2(nodeEdge2, "start", "end")
-		part2 <- len(a)
-	}()
-	// fmt.Println(a)
 
-	fmt.Println("Part 1", <-part1)
-	fmt.Println("Part 2", <-part2)
+	part1, part2 := 0, 0
+	eval := [][]string{{"end"}}
+
+	for len(eval) > 0 {
+		var path []string
+		path, eval = eval[len(eval)-1], eval[:len(eval)-1]
+		slicecnt := make(map[string]int)
+		hasSmall := false
+		for _, v := range path {
+			slicecnt[v]++
+			if slicecnt[v] > 1 {
+				if v[0] >= 'a' && v[0] <= 'z' {
+					hasSmall = true
+				}
+			}
+		}
+
+		end := path[0]
+		for _, k := range edge[end] {
+			c := slicecnt[k]
+			if k != "start" && k != "end" {
+				if k[0] >= 'a' && k[0] <= 'z' {
+					// Small cave
+					if c > 0 && hasSmall {
+						continue
+					}
+				}
+			} else if c > 0 {
+				continue
+			}
+			path2 := append([]string{k}, path...)
+
+			if k == "start" {
+				if !hasSmall {
+					part1++
+				}
+				part2++
+				continue
+			}
+			eval = append(eval, path2)
+		}
+	}
+
+	fmt.Println("Part 1", part1)
+	fmt.Println("Part 2", part2)
 }
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	// day12("test.txt")
 	// day12("test2.txt")
+	t1 := time.Now()
 	day12("input.txt")
+	d1 := time.Since(t1)
+	fmt.Println("Duration", d1)
 }
