03aa2087c74581bf6e4fcfe10b3478689642a965
Ang Chin Han <angch@tuxuri.com>
2021-12-13 13:59:41 +0800

    chore: Make it below 100 lines

--- angch/2021-13/main.go
+++ angch/2021-13/main.go
@@ -20,7 +20,6 @@ func day13(filepath string) {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	maxX, maxY := 0, 0
 
 	board := make(map[Coord]bool)
 	for scanner.Scan() {
@@ -31,36 +30,37 @@ func day13(filepath string) {
 		x, y := 0, 0
 		fmt.Sscanf(t, "%d,%d", &x, &y)
 		board[Coord{x, y}] = true
-		if y > maxY {
-			maxY = y
-		}
-		if x > maxX {
-			maxX = x
-		}
 	}
 
 	part1 := 0
+	maxX, maxY := 0, 0
 	for scanner.Scan() {
-		instr := scanner.Text()
-		instr = strings.ReplaceAll(instr, "fold along ", "")
-		a := strings.Split(instr, "=")
+		a := strings.Split(strings.TrimPrefix(scanner.Text(), "fold along "), "=")
 		fold, _ := strconv.Atoi(a[1])
-		xy := a[0]
+		fold2 := fold * 2
 
-		if xy == "y" {
+		if a[0] == "y" {
+			maxY = 0
 			for c := range board {
 				if c.Y >= fold {
-					y := fold - (c.Y - fold)
-					board[Coord{c.X, y}] = true
 					delete(board, c)
+					c.Y = fold2 - c.Y
+					board[c] = true
+				}
+				if c.Y > maxY {
+					maxY = c.Y
 				}
 			}
 		} else {
+			maxX = 0
 			for c := range board {
 				if c.X >= fold {
-					x := fold - (c.X - fold)
-					board[Coord{x, c.Y}] = true
 					delete(board, c)
+					c.X = fold2 - c.X
+					board[c] = true
+				}
+				if c.X > maxX {
+					maxX = c.X
 				}
 			}
 		}
@@ -70,15 +70,6 @@ func day13(filepath string) {
 	}
 	fmt.Println("Part 1", part1)
 
-	maxX, maxY = 0, 0
-	for c := range board {
-		if c.X > maxX {
-			maxX = c.X
-		}
-		if c.Y > maxY {
-			maxY = c.Y
-		}
-	}
 	for y := 0; y <= maxY; y++ {
 		for x := 0; x <= maxX; x++ {
 			if board[Coord{x, y}] {
@@ -86,7 +77,6 @@ func day13(filepath string) {
 			} else {
 				fmt.Print("..")
 			}
-
 		}
 		fmt.Println()
 	}
