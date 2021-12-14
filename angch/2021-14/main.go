1fc5988bd462098d99b33b3f6f0ba80c612c9a8a
Ang Chin Han <angch@tuxuri.com>
2021-12-14 18:36:18 +0800

    chore(2021-14/angch): Clean up

--- angch/2021-14/main.go
+++ angch/2021-14/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Edge struct {
-	from, to string
+	from, to byte
 }
 
 func day14(filepath string) {
@@ -27,67 +27,45 @@ func day14(filepath string) {
 	prev := 'x'
 	for _, v := range input {
 		if prev != 'x' {
-			counts[Edge{from: string(prev), to: string(v)}]++
+			counts[Edge{from: byte(prev), to: byte(v)}]++
 		}
 		prev = v
 	}
-
 	scanner.Scan()
-	scanner.Text()
 
-	rules := make(map[Edge]rune)
-
+	rules := make(map[Edge]byte)
 	for scanner.Scan() {
 		t := scanner.Text()
 		rule := strings.Split(t, " -> ")
-		from, to := string(rule[0][0]), string(rule[0][1])
+		from, to := byte(rule[0][0]), byte(rule[0][1])
 		add := rule[1][0]
-		rules[Edge{from: from, to: to}] = rune(add)
+		rules[Edge{from: from, to: to}] = byte(add)
 	}
-	total := 1
-	for _, v := range counts {
-		total += v
-	}
-	// fmt.Println("Step", 0, counts, total)
 	part1, part2 := 0, 0
 	for step := 0; step < 40; step++ {
 		newcounts := make(map[Edge]int)
 		for k, v := range counts {
 			newcounts[k] = v
 		}
-		for rule, to2 := range rules {
-			to := string(to2)
+		for rule, to := range rules {
 			c, ok := counts[Edge{from: rule.from, to: rule.to}]
 			if !ok {
 				continue
 			}
-			// fmt.Println("Rule", rule, to)
 			newcounts[Edge{from: rule.from, to: rule.to}] -= c
 			newcounts[Edge{from: rule.from, to: to}] += c
 			newcounts[Edge{from: to, to: rule.to}] += c
-			// fmt.Println(" ", newcounts)
 		}
 		counts = newcounts
 
-		total := 1
-		for k, v := range counts {
-			total += v
-			if v == 0 {
-				delete(counts, k)
-			}
-		}
-		// fmt.Println("Step", step+1, counts, total)
-
 		if step == 9 || step == 39 {
-			elem := make(map[string]int)
+			elem := make(map[byte]int)
 			for c, v := range counts {
 				elem[c.from] += v
 			}
-			elem[string(prev)]++
-			// fmt.Println(elem)
+			elem[byte(prev)]++
 
-			least := 999999999999999
-			most := -1
+			most, least := -1, 999999999999999
 			for _, v := range elem {
 				if most < v {
 					most = v
@@ -99,20 +77,16 @@ func day14(filepath string) {
 			if step == 9 {
 				part1 = most - least
 			} else {
-				//4019217425979
 				part2 = most - least
 			}
 		}
 	}
-	// fmt.Println("Step", step, counts)
-
 	fmt.Println("Part 1", part1)
 	fmt.Println("Part 2", part2)
 }
 
 func main() {
 	// day14("test.txt")
-
 	// For the purpose of running on machines without `time` aka Windows
 	t1 := time.Now()
 	day14("input.txt")
