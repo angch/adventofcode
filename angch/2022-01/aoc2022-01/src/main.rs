0cd220500a0437982d1b630d8e0f58cd25d898d1
Ang Chin Han <angch@tuxuri.com>
2022-12-02 10:14:13 +0800

    refac(2022-01): Make it more rusty.

--- angch/2022-01/aoc2022-01/src/main.rs
+++ angch/2022-01/aoc2022-01/src/main.rs
@@ -1,44 +1,35 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
 
 fn main() {
-    day1("../test.txt");
+    assert!(day1("../test.txt") == (24000, 41000));
     day1("../input.txt");
 }
-fn day1(filename: &str) {
+
+fn day1(filename: &str) -> (i32, i32) {
     println!("Day 1: {}", filename);
-    if let Ok(lines) = read_lines(filename) {
-        // Consumes the iterator, returns an (Optional) String
-        let mut elfs = Vec::new();
-        let mut cal: i32 = 0;
-        for line in lines {
-            if let Ok(t) = line {
-                if t == "" {
-                    // append to elfs
-                    elfs.push(-cal);
-                    cal = 0;
-                    continue;
-                }
-                let cal2 = t.parse::<i32>().unwrap();
-
-                // println!("{}", cal2);
-                cal = cal + cal2;
+    let mut elfs = Vec::new();
+    let mut cal = 0;
+    io::BufReader::new(File::open(filename).unwrap())
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>())
+        .for_each(|line| match line {
+            Err(_x) => {
+                elfs.push(-cal);
+                cal = 0;
             }
-        }
-        elfs.sort();
-        // println!("{:?}", elfs);
-        println!("Part 1: {}", -elfs[0]);
-        println!("Part 2: {}", -elfs[0] - elfs[1] - elfs[2]);
+            Ok(x) => {
+                cal += x;
+            }
+        });
+    if cal < 0 {
+        elfs.push(-cal);
     }
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    elfs.sort();
+    let part1 = -elfs[0];
+    // fancy way to say -elfs[0] - elfs[1] - elfs[2]
+    let part2 = -elfs.iter().take(3).sum::<i32>();
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+    (part1, part2)
 }
