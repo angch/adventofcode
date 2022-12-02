cf2b4f11ed78e81efbc0ad737f3dba7c9c54e537
Ang Chin Han <angch@tuxuri.com>
2022-12-02 17:10:16 +0800

    feat(2022-02): Optimized

--- angch/2022-02/main.go
+++ angch/2022-02/main.go
@@ -8,30 +8,6 @@ import (
 	"strings"
 )
 
-var win = map[string]int{
-	"AX": 3,
-	"AY": 6,
-	"AZ": 0,
-	"BX": 0,
-	"BY": 3,
-	"BZ": 6,
-	"CX": 6,
-	"CY": 0,
-	"CZ": 3,
-}
-
-var shapescore = map[string]int{
-	"X": 1,
-	"Y": 2,
-	"Z": 3,
-}
-
-var response = map[string][]string{
-	"A": {"Z", "X", "Y"},
-	"B": {"X", "Y", "Z"},
-	"C": {"Y", "Z", "X"},
-}
-
 func day2(file string) (int, int) {
 	part1, part2 := 0, 0
 	f, _ := os.Open(file)
@@ -41,30 +17,25 @@ func day2(file string) (int, int) {
 		t := scanner.Text()
 		lr := strings.Split(t, " ")
 		l, r := lr[0], lr[1]
+		r0 := int(r[0] - 'X')
+		l0 := int(l[0] - 'A')
 
-		roundscore := win[l+r] + shapescore[r]
-		part1 += roundscore
+		// Part 1
+		score1 := (l0+r0)%3*3 + r0 + 1
+		part1 += score1
 
-		i, j := 0, 0
-		switch r {
-		case "X":
-			i, j = 0, 0
-		case "Y":
-			i, j = 1, 3
-		case "Z":
-			i, j = 2, 6
-		}
-		resp := response[l][i]
-		part2 += shapescore[resp] + j
+		// Part 2
+		i := r0 * 3
+		part2 += i + (l0+r0)%3
 	}
 	return part1, part2
 }
 
 func main() {
 	part1, part2 := day2("test.txt")
+	fmt.Println(part1, part2)
 	if part1 != 15 || part2 != 12 {
 		log.Fatal("Test failed")
 	}
-	fmt.Println(part1, part2)
 	fmt.Println(day2("input.txt"))
 }
