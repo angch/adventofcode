94db75cda373dc6f9f7bb57b6db0aaec2d785177
Ang Chin Han <angch@tuxuri.com>
2022-12-02 17:23:01 +0800

    refac(2022-02): tighter

--- angch/2022-02/main.go
+++ angch/2022-02/main.go
@@ -16,17 +16,9 @@ func day2(file string) (int, int) {
 	for scanner.Scan() {
 		t := scanner.Text()
 		lr := strings.Split(t, " ")
-		l, r := lr[0], lr[1]
-		r0 := int(r[0] - 'X')
-		l0 := int(l[0] - 'A')
-
-		// Part 1
-		score1 := (l0+r0)%3*3 + r0 + 1
-		part1 += score1
-
-		// Part 2
-		i := r0 * 3
-		part2 += i + (l0+r0)%3
+		l, r := int(lr[0][0]-'A'), int(lr[1][0]-'X')
+		part1 += (l+r)%3*3 + r + 1
+		part2 += r*3 + (l+r)%3
 	}
 	return part1, part2
 }
