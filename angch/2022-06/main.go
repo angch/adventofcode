2e96c4efb37dcda423cc78c44bcd47572d0a9dec
Ang Chin Han <angch@tuxuri.com>
2022-12-06 13:18:05 +0800

    refac(2022-06): Minor cleanup

--- angch/2022-06/main.go
+++ angch/2022-06/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -17,40 +18,30 @@ func day6(file string) (int, int) {
 	for scanner.Scan() {
 		t := scanner.Text()
 
-		seen := make([]byte, 0, 4)
-		seen2 := make([]byte, 0, 14)
+		seen := make([]byte, 0, 14)
 		for i := 0; i < len(t); i++ {
-			if len(seen) == 4 {
+			if len(seen) >= 4 {
+				k := make(map[byte]bool)
+				for _, v := range seen[len(seen)-4:] {
+					k[v] = true
+				}
+				if len(k) == 4 && part1 == 0 {
+					part1 = i
+				}
+			}
+			if len(seen) == 14 {
 				k := make(map[byte]bool)
 				for _, v := range seen {
 					k[v] = true
 				}
-				if len(k) == 4 && part1 == 0 {
-					// fmt.Println("part1", i)
-					part1 = i
-					// break
-
-				}
-			}
-			if len(seen2) == 14 {
-				k := make(map[byte]bool)
-				for _, v := range seen2 {
-					k[v] = true
-				}
 				if len(k) == 14 && part2 == 0 {
-					// fmt.Println("part2", i)
 					part2 = i
 					break
 				}
 			}
-
 			seen = append(seen, t[i])
-			seen2 = append(seen2, t[i])
-			if len(seen) > 4 {
+			if len(seen) > 14 {
 				seen = seen[1:]
-			}
-			if len(seen2) > 14 {
-				seen2 = seen2[1:]
 			}
 		}
 	}
@@ -61,8 +52,8 @@ func day6(file string) (int, int) {
 func main() {
 	part1, part2 := day6("test.txt")
 	fmt.Println(part1, part2)
-	// if part1 != "CMZ" || part2 != "MCD" {
-	// 	log.Fatal("Test failed")
-	// }
+	if part1 != 5 || part2 != 23 { // Tested first case only
+		log.Fatal("Test failed")
+	}
 	fmt.Println(day6("input.txt"))
 }
