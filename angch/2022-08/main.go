4e9419c4e138a87ae5b8be53edf269149d837114
Ang Chin Han <angch@tuxuri.com>
2022-12-08 13:49:00 +0800

    refac(2022-08): Use directions

--- angch/2022-08/main.go
+++ angch/2022-08/main.go
@@ -11,6 +11,8 @@ var verbose = false
 
 type coord [2]int
 
+var directions = []coord{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
 func day8(file string) (int, int) {
 	part1, part2 := 0, 0
 	f, _ := os.Open(file)
@@ -22,111 +24,42 @@ func day8(file string) (int, int) {
 	y := 0
 	for scanner.Scan() {
 		t := scanner.Text()
-
 		for x, v := range t {
 			c := coord{x, y}
 			grid[c] = int(v) - '0'
 		}
 		y++
-
-		if verbose {
-
-		}
-	}
-	if verbose {
-		fmt.Println(grid)
 	}
 
 	bestscore := 0
-	for y1 := 0; y1 < y; y1++ {
-		for x1 := 0; x1 < y; x1++ {
-			c := coord{x1, y1}
-			h := grid[c]
-			score := 1
-
-			dist := 0
-			no := false
-			for x2 := x1 - 1; x2 >= 0; x2-- {
-				c2 := coord{x2, y1}
-				dist++
-				if grid[c2] >= h {
-					no = true
+	for c := range grid {
+		score, donepart1 := 1, false
+		for _, d := range directions {
+			dist, isblocked := 0, false
+			c1, h := c, grid[c]
+			for {
+				c1 = coord{c1[0] + d[0], c1[1] + d[1]}
+				h2, ok := grid[c1]
+				if !ok {
 					break
 				}
-			}
-			// log.Println("dist is", dist)
-			score *= dist
-			// log.Println("dist is", dist, score)
-
-			done1 := false
-			if !no {
-				part1++
-				// log.Println("found", c, h, "left")
-				done1 = true
-			}
-
-			no = false
-			dist = 0
-			for x2 := x1 + 1; x2 < y; x2++ {
-				c2 := coord{x2, y1}
 				dist++
-				if grid[c2] >= h {
-					no = true
+				if h2 >= h {
+					isblocked = true
 					break
 				}
 			}
 			score *= dist
-			// log.Println("dist is", dist, score)
-			if !no && !done1 {
+			if !isblocked && !donepart1 {
+				donepart1 = true
 				part1++
-				// log.Println("found", c, h, "right")
-				done1 = true
 			}
-			no = false
-			dist = 0
-			for y2 := y1 - 1; y2 >= 0; y2-- {
-				c2 := coord{x1, y2}
-				dist++
-				if grid[c2] >= h {
-					no = true
-					break
-				}
-				// dist++
-			}
-			score *= dist
-			// log.Println("dist is", dist, score)
-			if !no && !done1 {
-				part1++
-				done1 = true
-				// continue
-			}
-
-			no = false
-			dist = 0
-			for y2 := y1 + 1; y2 < y; y2++ {
-				c2 := coord{x1, y2}
-				dist++
-				if grid[c2] >= h {
-					no = true
-					break
-				}
-			}
-			// log.Println("dist is", dist, score)
-			score *= dist
-			if !no && !done1 {
-				part1++
-				// continue
-				done1 = true
-			}
-
-			// log.Println("score is", score, x1, y1)
-			if score > bestscore {
-				bestscore = score
-			}
+		}
+		if score > bestscore {
+			bestscore = score
 		}
 	}
 	part2 = bestscore
-
 	return part1, part2
 }
 
