0aab5fb74f0b2440351adbbd9d8cd26a1d26cae0
Ang Chin Han <angch@tuxuri.com>
2022-12-08 13:50:29 +0800

    refac(2022-08): Use directions

--- angch/2022-08/main.go
+++ angch/2022-08/main.go
@@ -7,8 +7,6 @@ import (
 	"os"
 )
 
-var verbose = false
-
 type coord [2]int
 
 var directions = []coord{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
@@ -19,14 +17,12 @@ func day8(file string) (int, int) {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
-	// curdir, cmd := "", ""
 	grid := make(map[coord]int)
 	y := 0
 	for scanner.Scan() {
 		t := scanner.Text()
 		for x, v := range t {
-			c := coord{x, y}
-			grid[c] = int(v) - '0'
+			grid[coord{x, y}] = int(v) - '0'
 		}
 		y++
 	}
@@ -35,8 +31,8 @@ func day8(file string) (int, int) {
 	for c := range grid {
 		score, donepart1 := 1, false
 		for _, d := range directions {
-			dist, isblocked := 0, false
 			c1, h := c, grid[c]
+			dist, isblocked := 0, false
 			for {
 				c1 = coord{c1[0] + d[0], c1[1] + d[1]}
 				h2, ok := grid[c1]
