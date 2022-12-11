e2aa7e5e5b092fb85744bf463407c09b9074ded9
Ang Chin Han <ang.chin.han@gmail.com>
2022-12-11 16:45:53 +0800

    refac(2022-12): Clean up

--- angch/2022-11/main.go
+++ angch/2022-11/main.go
@@ -9,7 +9,6 @@ import (
 )
 
 type monkey struct {
-	i           int
 	items       []int
 	op1         string
 	op2         int
@@ -29,33 +28,26 @@ func day11(file string) (int, int) {
 			continue
 		}
 		m := monkey{}
-		fmt.Sscanf(t, "Monkey %d:", &m.i)
-
 		scanner.Scan()
-		t = scanner.Text()
-		_, r, _ := strings.Cut(t, ": ")
+		_, r, _ := strings.Cut(scanner.Text(), ": ")
 		items := strings.Split(r, ", ")
 		m.items = make([]int, len(items))
 		for i, item := range items {
 			fmt.Sscanf(item, "%d", &m.items[i])
 		}
 		scanner.Scan()
-		t = scanner.Text()
-		_, r, _ = strings.Cut(t, " old ")
+		_, r, _ = strings.Cut(scanner.Text(), " old ")
 		words := strings.Split(r, " ")
 		fmt.Sscanf(r, "%s %d", &m.op1, &m.op2)
 		if len(words) > 1 && words[1] == "old" {
 			m.op2 = -999
 		}
 		scanner.Scan()
-		t = scanner.Text()
-		fmt.Sscanf(t, "  Test: divisible by %d", &m.testdiv)
+		fmt.Sscanf(scanner.Text(), "  Test: divisible by %d", &m.testdiv)
 		scanner.Scan()
-		t = scanner.Text()
-		fmt.Sscanf(t, " If true: throw to monkey %d", &m.trueaction)
+		fmt.Sscanf(scanner.Text(), " If true: throw to monkey %d", &m.trueaction)
 		scanner.Scan()
-		t = scanner.Text()
-		fmt.Sscanf(t, " If false: throw to monkey %d", &m.falseaction)
+		fmt.Sscanf(scanner.Text(), " If false: throw to monkey %d", &m.falseaction)
 		monkeys = append(monkeys, m)
 	}
 	part1 := make(chan int)
@@ -76,21 +68,13 @@ func do(monkeysInput []monkey, r int, div3 bool) int {
 		div *= v.testdiv
 		monkeys[k] = monkeysInput[k]
 	}
-
 	monkeyCount := make([]int, len(monkeys))
-	verbose := false
 
 	for rounds := 0; rounds < r; rounds++ {
 		for k, m := range monkeys {
-			if verbose {
-				fmt.Printf("Monkey %d\n", m.i)
-			}
-			for k, v := range m.items {
-				monkeyCount[m.i]++
-				if verbose {
-					fmt.Println(" Monkey inspects item with level", v)
-				}
-				worry, op2 := v, m.op2
+			for _, worry := range m.items {
+				monkeyCount[k]++
+				op2 := m.op2
 				if m.op2 == -999 {
 					op2 = worry
 				}
@@ -99,41 +83,22 @@ func do(monkeysInput []monkey, r int, div3 bool) int {
 				} else if m.op1 == "*" {
 					worry *= op2
 				}
-				if verbose {
-					fmt.Printf("  Worry level is now %d for item %d\n", worry, k)
-				}
 
 				if div3 {
 					worry /= 3
 				} else {
 					worry %= div
 				}
-				if verbose {
-					fmt.Printf("  Worry level is now %d for item %d\n", worry, k)
-				}
-				if !div3 {
-					worry %= div
-				}
+
 				target := m.falseaction
 				if worry%m.testdiv == 0 {
 					target = m.trueaction
 				}
 				monkeys[target].items = append(monkeys[target].items, worry)
-				if verbose {
-					fmt.Printf("  Throwing item %d to monkey %d\n", worry, target)
-				}
 			}
 			monkeys[k].items = []int{}
 		}
 	}
-
-	if verbose {
-		for _, m := range monkeys {
-			fmt.Println("Monkey", m.i, m.items)
-		}
-		fmt.Println(monkeyCount)
-	}
-
 	sort.Ints(monkeyCount)
 	return monkeyCount[len(monkeyCount)-1] * monkeyCount[len(monkeyCount)-2]
 }
