92a3cdba9ea9f3911f3f218870f8e76a84e646db
Ivan Tham <pickfire@riseup.net>
2022-12-15 00:40:01 +0800

    Add pickfire day 11 part 2

--- 2022-11/pickfire/src/main.rs
+++ 2022-11/pickfire/src/main.rs
@@ -12,8 +12,7 @@ enum Op {
 
 #[derive(Debug)]
 struct Monkey {
-    items1: Vec<usize>,
-    items2: Vec<usize>,
+    items: [Vec<usize>; 2],
     operation: Op,
     test: usize,
     test_pass: usize,
@@ -47,72 +46,62 @@ fn main() {
         let test_fail = test_fail.split("monkey ").nth(1).unwrap().parse().unwrap();
         lines.next();
         monkeys.push(Monkey {
-            items1: items.clone(),
-            items2: items,
+            items: [items.clone(), items],
             operation,
             test,
             test_pass,
             test_fail,
         });
     }
-    let mut inspects1 = vec![0; monkeys.len()];
-    let mut inspects2 = vec![0; monkeys.len()];
+    let mut inspects = vec![vec![0; monkeys.len()]; 2];
     let mut part1 = 0usize;
+    let lcm: usize = monkeys.iter().map(|m| m.test).product();
     for n in 0..10000 {
         for i in 0..monkeys.len() {
-            for item in std::mem::take(&mut monkeys[i].items1) {
+            let item_value = |item, operation| {
+                let val = |v| match v {
+                    Value::Constant(n) => n,
+                    Value::Variable => item,
+                };
+                match operation {
+                    Op::Add(x, y) => val(x) + val(y),
+                    Op::Mul(x, y) => val(x) * val(y),
+                }
+            };
+            let next_monkey = |item, monkey: &Monkey| {
+                if item % monkey.test == 0 {
+                    monkey.test_pass
+                } else {
+                    monkey.test_fail
+                }
+            };
+            for mut item in std::mem::take(&mut monkeys[i].items[0]) {
                 if n > 19 {
                     continue;
                 }
-                let val = |v| match v {
-                    Value::Constant(n) => n,
-                    Value::Variable => item,
-                };
-                let item = match monkeys[i].operation {
-                    Op::Add(x, y) => val(x) + val(y),
-                    Op::Mul(x, y) => val(x) * val(y),
-                } / 3;
-                inspects1[i] += 1;
-                let next_monkey = if item % monkeys[i].test == 0 {
-                    monkeys[i].test_pass
-                } else {
-                    monkeys[i].test_fail
-                };
-                monkeys[next_monkey].items1.push(item);
+                item = item_value(item, monkeys[i].operation) / 3;
+                inspects[0][i] += 1;
+                let next_monkey = next_monkey(item, &monkeys[i]);
+                monkeys[next_monkey].items[0].push(item);
             }
-            for item in std::mem::take(&mut monkeys[i].items2) {
-                let val = |v| match v {
-                    Value::Constant(n) => n,
-                    Value::Variable => item,
-                };
-                let item = match monkeys[i].operation {
-                    Op::Add(x, y) => val(x) + val(y),
-                    Op::Mul(x, y) => {
-                        let (z, over) = val(x).overflowing_mul(val(y));
-                        if over {
-                            println!("over");
-                        }
-                        z
-                    }
-                };
-                inspects2[i] += 1;
-                let next_monkey = if item % monkeys[i].test == 0 {
-                    monkeys[i].test_pass
-                } else {
-                    monkeys[i].test_fail
-                };
-                monkeys[next_monkey].items2.push(item);
+            for mut item in std::mem::take(&mut monkeys[i].items[1]) {
+                item = item_value(item, monkeys[i].operation);
+                inspects[1][i] += 1;
+                let next_monkey = next_monkey(item, &monkeys[i]);
+                monkeys[next_monkey].items[1].push(item);
+            }
+        }
+        for monkey in &mut monkeys {
+            for item in &mut monkey.items[1] {
+                *item %= lcm;
             }
         }
         if n == 19 {
-            inspects1.sort_unstable();
-            part1 = inspects1[inspects1.len() - 2..].iter().product();
-        }
-        if n == 0 || n == 19 || (n > 999 && (n + 1) % 1000 == 0) {
-            println!("{n} {inspects2:?}");
+            inspects[0].sort_unstable();
+            part1 = inspects[0][inspects[0].len() - 2..].iter().product();
         }
     }
-    inspects2.sort_unstable();
-    let part2: usize = inspects2[inspects2.len() - 2..].iter().product();
+    inspects[1].sort_unstable();
+    let part2: usize = inspects[1][inspects[1].len() - 2..].iter().product();
     println!("{part1} {part2}");
 }
