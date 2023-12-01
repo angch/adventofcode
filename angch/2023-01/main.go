ffdbc9eaa097da3f2b9d8f940f5c92d2014a8d95
Ang Chin Han <angch@tuxuri.com>
2023-12-01 14:21:23 +0800

    refac(2023-01): angch does not know about LastIndex

--- angch/2023-01/main.go
+++ angch/2023-01/main.go
@@ -12,65 +12,14 @@ var words = []string{
 	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 }
 
-var words2 = []string{
-	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-}
-
-func reverse(t2 string) string {
-	t := []rune(t2)
-	for i := 0; i < len(t)/2; i++ {
-		t[i], t[len(t)-i-1] = t[len(t)-i-1], t[i]
-	}
-	return string(t)
-}
-
-func init() {
-	for k := range words2 {
-		words2[k] = reverse(words2[k])
-	}
-}
-
-func day1(file1, file2 string) (int, int) {
-	return day1part1(file1), day1part2(file2)
-}
-
-func day1part1(file string) int {
-	part1 := 0
+func day1(file string) (int, int) {
+	part1, part2 := 0, 0
 	f, _ := os.Open(file)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
 	for scanner.Scan() {
 		t := scanner.Text()
-		first, last := -1, -1
-		for _, v := range t {
-			if v >= '0' && v <= '9' {
-				if first < 0 {
-					first = int(v) - '0'
-				} else {
-					last = int(v) - '0'
-				}
-			}
-		}
-		if last == -1 {
-			last = first
-		}
-		part1 += first*10 + last
-		_ = t
-	}
-
-	return part1
-}
-
-func day1part2(file string) int {
-	part2 := 0
-	f, _ := os.Open(file)
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-
-	for scanner.Scan() {
-		t := scanner.Text()
-		// first := (t[0]-'0')*10 + t[len(t)-1] - '0'
 		first, last := -1, -1
 		firstk, lastk := 999, -1
 		for k, v := range t {
@@ -88,6 +37,7 @@ func day1part2(file string) int {
 			last = first
 			lastk = firstk
 		}
+		part1 += first*10 + last
 
 		for k, v := range words {
 			i := strings.Index(t, v)
@@ -95,18 +45,12 @@ func day1part2(file string) int {
 				first = k
 				firstk = i
 			}
-		}
-
-		// Too annoying to scan until not found, just reverse the thing so we scan backwards
-		t2 := reverse(t)
-		for k, v := range words2 {
-			i := strings.Index(t2, v)
-			if i >= 0 && len(t)-i > lastk {
+			i = strings.LastIndex(t, v)
+			if i >= 0 && i > lastk {
 				last = k
-				lastk = len(t) - i
+				lastk = i
 			}
 		}
-		// fmt.Println(first, last, t) // So much easy to eyeball debug for correctness
 		part2 += first*10 + last
 
 		// Sanity
@@ -114,16 +58,15 @@ func day1part2(file string) int {
 			log.Fatal("OOB")
 		}
 	}
-
-	return part2
+	return part1, part2
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	part1, part2 := day1("test.txt", "test2.txt")
+	part1, _ := day1("test.txt")
+	_, part2 := day1("test2.txt")
 	if part1 != 142 || part2 != 281 {
-		log.Fatal("Test failed")
+		log.Fatal("Test failed ", part1, part2)
 	}
-
-	fmt.Println(day1("input.txt", "input.txt"))
+	fmt.Println(day1("input.txt"))
 }
