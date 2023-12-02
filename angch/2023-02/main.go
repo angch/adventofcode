4c875d28f3d9486e44922661fb8a8a282cf6b89c
Ang Chin Han <angch@tuxuri.com>
2023-12-02 14:11:40 +0800

    refac(2023-02): Clean up

--- angch/2023-02/main.go
+++ angch/2023-02/main.go
@@ -10,7 +10,7 @@ import (
 
 func day2(file string) (int, int) {
 	part1, part2 := 0, 0
-	r, g, b := 12, 13, 14
+	limit := map[string]int{"red": 12, "green": 13, "blue": 14}
 	f, _ := os.Open(file)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
@@ -18,51 +18,28 @@ func day2(file string) (int, int) {
 	gameid := 0
 	for scanner.Scan() {
 		t := scanner.Text()
-		_ = t
 		gameid++
 
-		l1, r2, _ := strings.Cut(t, ":")
+		_, r2, _ := strings.Cut(t, ":")
 		sets := strings.Split(r2, ";")
 
 		invalid := false
-		red, green, blue := 0, 0, 0
-		for k, v := range sets {
-
+		l2 := make(map[string]int)
+		for _, v := range sets {
 			cubes := strings.Split(v, ",")
 			for _, c := range cubes {
 				num, col := 0, ""
 				fmt.Sscanf(c, "%d %s", &num, &col)
-				if false {
-					fmt.Println(l1, k, num, col)
-				}
-				switch col {
-				case "blue":
-					blue = max(blue, num)
-					if num > b {
-						invalid = true
-					}
-				case "red":
-					red = max(red, num)
-					if num > r {
-						invalid = true
-					}
-				case "green":
-					green = max(green, num)
-					if num > g {
-						invalid = true
-					}
-				default:
-					log.Fatal(col)
+				l2[col] = max(l2[col], num)
+				if num > limit[col] {
+					invalid = true
 				}
 			}
 		}
-		power := red * green * blue
-		log.Println("Game ", gameid, invalid, power, red, green, blue)
 		if !invalid {
 			part1 += gameid
 		}
-		part2 += power
-
+		part2 += l2["red"] * l2["green"] * l2["blue"]
 	}
 	return part1, part2
 }
@@ -70,7 +47,6 @@ func day2(file string) (int, int) {
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	part1, part2 := day2("test.txt")
-	fmt.Println(part1, part2)
 	if part1 != 8 || part2 != 2286 {
 		log.Fatal("Test failed ", part1, part2)
 	}
