b8f9e0be283d91b6760aa6ee1179d71d867d7367
Ang Chin Han <angch@tuxuri.com>
2024-12-02 12:09:16 +0800

    feat(angch): 2024-01

---
+++ angch/2024-01/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
+
+func day1(file string) (part1, part2 int) {
+	f, _ := os.Open(file)
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	one := []int{}
+	two := []int{}
+	count := map[int]int{}
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		n1, n2 := 0, 0
+		fmt.Sscanf(t, "%d %d", &n1, &n2)
+		one = append(one, n1)
+		two = append(two, n2)
+		count[n2]++
+	}
+	sort.Ints(one)
+	sort.Ints(two)
+	for k, v := range one {
+		d := two[k] - v
+		if d < 0 {
+			d = -d
+		}
+		part1 += d
+		part2 += v * count[v]
+	}
+	return
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	part1, part2 := day1("test.txt")
+	log.Println(part1)
+	if part1 != 11 || part2 != 31 {
+		log.Fatal("Test failed ", part1, part2)
+	}
+	fmt.Println(day1("input.txt"))
+}
