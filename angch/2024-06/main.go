e4a84c0273eb800874f4ec01990601a3743e9757
Ang Chin Han <ang.chin.han@gmail.com>
2024-12-06 13:52:07 +0800

    refac(angch/2024-06): Use sparsemap

--- angch/2024-06/main.go
+++ angch/2024-06/main.go
@@ -15,21 +15,23 @@ func day6(file string) (part1, part2 int) {
 	}
 	defer f.Close()
 
-	mm := make(map[[2]int]byte)
+	mm := make(map[[2]int]bool)
 	guard := [2]int{0, 0}
 
 	scanner := bufio.NewScanner(f)
 	y := 0
+	maxX := 0
 	for scanner.Scan() {
 		t := scanner.Text()
 		for x, v := range t {
-			if v != 'p' {
-				mm[[2]int{x, y}] = byte(v)
+			if v == '#' {
+				mm[[2]int{x, y}] = true
 			}
 			if v == '^' {
 				guard = [2]int{x, y}
 			}
 		}
+		maxX = len(t)
 		y++
 	}
 	// log.Println(mm)
@@ -42,10 +44,10 @@ func day6(file string) (part1, part2 int) {
 	visited[guard] = true
 	for {
 		guard2 := [2]int{guard[0] + dir[0], guard[1] + dir[1]}
-		if mm[guard2] == '.' || mm[guard2] == '^' {
+		if !mm[guard2] && guard2[0] >= 0 && guard2[0] < maxX && guard2[1] >= 0 && guard2[1] < y {
 			guard = guard2
 			visited[guard] = true
-		} else if mm[guard2] == '#' {
+		} else if mm[guard2] {
 			// turn right
 			dir = [2]int{-dir[1], dir[0]}
 		} else {
@@ -65,7 +67,7 @@ a:
 
 		for {
 			guard2 := [2]int{guard[0] + dir[0], guard[1] + dir[1]}
-			if guard2 != v && mm[guard2] == '.' || mm[guard2] == '^' {
+			if guard2 != v && !mm[guard2] && guard2[0] >= 0 && guard2[0] < maxX && guard2[1] >= 0 && guard2[1] < y {
 				guard = guard2
 
 				if (visited2[guard] & (1 << dirIndex)) != 0 {
@@ -73,7 +75,7 @@ a:
 					continue a
 				}
 				visited2[guard] |= 1 << dirIndex
-			} else if mm[guard2] == '#' || guard2 == v {
+			} else if mm[guard2] || guard2 == v {
 				// turn right
 				dir = [2]int{-dir[1], dir[0]}
 				dirIndex = (dirIndex + 1) % 4
