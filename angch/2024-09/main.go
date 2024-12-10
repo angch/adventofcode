919eb9276073102fdbce4fe0a1b255a1a7cf49bc
Ang Chin Han <ang.chin.han@gmail.com>
2024-12-10 20:56:41 +0800

    refac(day9): Remove commented debug statements for cleaner code

--- angch/2024-09/main.go
+++ angch/2024-09/main.go
@@ -45,14 +45,12 @@ func day9(file string) (part1, part2 int) {
 		}
 		files = id
 	}
-	// fmt.Println(spans)
 	spans2 := make([]diskstruct, len(spans))
 	copy(spans2, spans)
 
 	for {
 		l, r := 0, len(spans)-1
 		for ; l < r && spans[l].id != -1; l++ {
-			// Find empty slot
 		}
 		for ; l < r && spans[r].id == -1; r-- {
 		}
@@ -62,26 +60,18 @@ func day9(file string) (part1, part2 int) {
 
 		spans[l].id = spans[r].id
 		if spans[l].len > spans[r].len {
-			// fmt.Println("lr1", l, r)
 			spaceleft := spans[l].len - spans[r].len
 			spans[l].len = spans[r].len
 			spans = append(spans[:l+1], append([]diskstruct{{id: -1, len: spaceleft}}, spans[l+1:]...)...)
 			spans = append(spans[:r+1], spans[r+2:]...)
 		} else if spans[l].len < spans[r].len {
-			// fmt.Println("lr2", l, r)
 			spans[r].len -= spans[l].len
-			// spans[l].len = spans[r].len
 		} else {
-			// fmt.Println("lr3", l, r, spans[l], spans[r])
 			spans = append(spans[:r], spans[r+1:]...)
 		}
-		// fmt.Println(spans)
 		if spans[len(spans)-1].id == -1 {
-			// fmt.Println("rmtail")
 			spans = spans[:len(spans)-1]
-			// fmt.Println(spans)
 		}
-
 	}
 
 	// fmt.Println(spans)
@@ -109,23 +99,16 @@ func day9(file string) (part1, part2 int) {
 
 	for i := files - 1; i >= 0; i-- {
 		for j := 0; j < len(fsm); j++ {
-			// if i == 2 {
-			// 	log.Println("p2,", i, j, fsm[j].pos, idx[i])
-			// 	log.Println(fsm, idx)
-			// }
 			if idx[i].len <= fsm[j].len && idx[i].pos > fsm[j].pos {
 				idx[i].pos = fsm[j].pos
 				fsm[j].pos += idx[i].len
 				fsm[j].len -= idx[i].len
-				// log.Println(fsm, idx)
 				break
 			}
 		}
 	}
-	// log.Println(idx)
 
 	for _, v := range idx {
-		// log.Println(*v)
 		pos := v.pos
 		for range v.len {
 			part2 += pos * v.id
