b12527c4b21231b2243da72d9d45037cfe4632d8
Ang Chin Han <ang.chin.han@gmail.com>
2024-12-12 17:28:34 +0800

    refact(day11): Simplify stone processing logic and update return types

--- angch/2024-11/main.go
+++ angch/2024-11/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"log"
 	"os"
@@ -11,80 +10,12 @@ import (
 	"time"
 )
 
-// func split(i int) (int, int) {
-// 	counts := 0
-// 	mult := 1
-// 	for j := i; j > 0; j /= 10 {
-// 		counts++
-// 		mult *= 10
-// 	}
-// 	if counts%2 == 1 {
-// 		return i, 0
-// 	}
-
-// 	return l, r
-// }
-
-var maxl = 0
-
-var cycles = map[int][]int{
-	0:     {1},       // -1 1
-	1:     {2024},    // -2 1
-	2024:  {20, 24},  // -3 2
-	20:    {2, 0},    // -4 4
-	24:    {2, 4},    // -4 4
-	2:     {4048},    // -6
-	4:     {8096},    // -7
-	4048:  {40, 48},  // -8
-	8096:  {80, 96},  // -9
-	40:    {4, 0},    // -10
-	48:    {4, 8},    // -10
-	80:    {8, 0},    // -10
-	96:    {9, 6},    // -10
-	8:     {16192},   // -12
-	9:     {18144},   // -13
-	16192: {161, 92}, // -14
-	18144: {181, 44}, // -15
-	161:   {16, 1},   // -16
-	92:    {9, 2},    // -16
-
-	44: {4, 4},  // -16
-	16: {32384}, // -18
-	18: {36336}, // -19
-
-}
-
-// 20 24
-// 2 0 2 4
-// 0 0 0 0
-
-func dostone(stones *list.List) {
-	for i := stones.Front(); i != nil; i = i.Next() {
-		if i.Value == 0 {
-			i.Value = 1
-			continue
-		}
-
-		ten := fmt.Sprintf("%d", i.Value.(int))
-		isEvenTen := len(ten)%2 == 0
-		if isEvenTen {
-			l, _ := strconv.Atoi(ten[:len(ten)/2])
-			r, _ := strconv.Atoi(ten[len(ten)/2:])
-			i.Value = l
-			i = stones.InsertAfter(r, i)
-			if len(ten) > maxl {
-				maxl = len(ten)
-				fmt.Println("Max length", maxl)
-			}
-			continue
-		}
-		i.Value = i.Value.(int) * 2024
-	}
-}
-
-func dostone2(stones map[int]uint) map[int]uint {
-	out := make(map[int]uint)
+func dostone(stones map[int]int) (map[int]int, int) {
+	out := make(map[int]int)
+	total := 0
 	for stone, count := range stones {
+		total += count
+
 		if stone == 0 {
 			out[1] += count
 			continue
@@ -97,15 +28,15 @@ func dostone2(stones map[int]uint) map[int]uint {
 			r, _ := strconv.Atoi(ten[len(ten)/2:])
 			out[l] += count
 			out[r] += count
+			total += count
 			continue
 		}
-
 		out[stone*2024] += count
 	}
-	return out
+	return out, total
 }
 
-func day11(file string, parttwo bool) (part1, part2 uint) {
+func day11(file string, parttwo bool) (part1, part2 int) {
 	f, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
@@ -113,38 +44,27 @@ func day11(file string, parttwo bool) (part1, part2 uint) {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
-	stones := list.New()
-	stones2 := make(map[int]uint)
+	stones := make(map[int]int)
 	for scanner.Scan() {
-		t := scanner.Text()
-		f := strings.Fields(t)
-		for _, v := range f {
+		for _, v := range strings.Fields(scanner.Text()) {
 			i, _ := strconv.Atoi(v)
-			stones.PushBack(i)
-			stones2[i]++
+			stones[i]++
 		}
 	}
 
+	count := 0
 	for times := range 25 {
-		// dostone(stones)
-		stones2 = dostone2(stones2)
-		fmt.Println(times+1, len(stones2), maxl)
+		stones, count = dostone(stones)
+		if times == 24 {
+			part1 = count
+		}
 	}
-	// part1 = len(stones2)
-	for _, v := range stones2 {
-		part1 += v
 
-	}
-	// if !parttwo {
-	// 	return
-	// }
 	for times := range 75 - 25 {
-		// dostone(stones)
-		stones2 = dostone2(stones2)
-		fmt.Println(times+26, len(stones2), maxl)
-	}
-	for _, v := range stones2 {
-		part2 += v
+		stones, count = dostone(stones)
+		if times == 75-25-1 {
+			part2 = count
+		}
 	}
 
 	return
@@ -158,9 +78,6 @@ func main() {
 	if part1 != 55312 {
 		log.Fatal("Test failed ", part1, part2)
 	}
-	// too low  207957094518723
-	// too low  207961465400585
-	// too high 315840075824712
 	fmt.Println(day11("input.txt", true))
 	fmt.Println("Elapsed time:", time.Since(t1))
 }
