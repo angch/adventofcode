eb6dda2c1953a9724d303be19f3691d2c89afe00
Ang Chin Han <ang.chin.han@gmail.com>
2024-12-13 13:55:49 +0800

    refac(angch/2024-13): Clean up

--- angch/2024-13/main.go
+++ angch/2024-13/main.go
@@ -11,13 +11,6 @@ import (
 	"time"
 )
 
-func gcd(a, b int) int {
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
-}
-
 // Function to calculate the determinant of a 2x2 matrix
 func det(a, b, c, d int) int {
 	return a*d - b*c
@@ -48,14 +41,11 @@ func day13(file string) (part1, part2 int) {
 		// Parse: Button A: X+31, Y+16
 
 		m := regex.FindStringSubmatch(l1)
-		// log.Printf("%+v", m)
 		b1, _ = strconv.Atoi(m[2])
 		c1, _ = strconv.Atoi(m[3])
-
 		buttons = append(buttons, [2]int{b1, c1})
 
 		m = regex.FindStringSubmatch(l2)
-		// a = m[1]
 		b1, _ = strconv.Atoi(m[2])
 		c1, _ = strconv.Atoi(m[3])
 		buttons = append(buttons, [2]int{b1, c1})
@@ -63,53 +53,38 @@ func day13(file string) (part1, part2 int) {
 		m = regex2.FindStringSubmatch(l3)
 		b1, _ = strconv.Atoi(m[1])
 		c1, _ = strconv.Atoi(m[2])
-
 		prize := [2]int{b1, c1}
 
-		log.Println(buttons)
-		log.Println("Prize", prize)
-
-		log.Println(gcd(buttons[0][0], prize[0]))
-		buttona := buttons[0]
-		buttonb := buttons[1]
-
-		// sum := 0
-		x1, x2 := buttona[0], buttonb[0]
-		y1, y2 := buttona[1], buttonb[1]
+		x1, x2 := buttons[0][0], buttons[1][0]
+		y1, y2 := buttons[0][1], buttons[1][1]
 		x3, y3 := prize[0], prize[1]
+
 		// Calculate the determinant of the coefficient matrix
 		D := det(x1, x2, y1, y2)
-		if D == 0 {
-			fmt.Println("No unique solution exists.")
-		} else {
-			// Calculate the determinants for a and b
-			Da := det(x3, x2, y3, y2)
-			Db := det(x1, x3, y1, y3)
-			// Solve for a and b using Cramer's rule
-			a := Da / D
-			b := Db / D
-
-			if a*x1+b*x2 == x3 && a*y1+b*y2 == y3 {
-				fmt.Printf("The solution is: a = %d, b = %d\n", a, b)
-				part1 += a*3 + b
-			} else {
-				fmt.Println("The solution is incorrect.")
-			}
-		}
-
-		x3, y3 = x3+10000000000000, y3+10000000000000
 		// Calculate the determinants for a and b
 		Da := det(x3, x2, y3, y2)
 		Db := det(x1, x3, y1, y3)
+
 		// Solve for a and b using Cramer's rule
 		a := Da / D
 		b := Db / D
 
-		if a*x1+b*x2 == x3 && a*y1+b*y2 == y3 {
-			fmt.Printf("The solution is: a = %d, b = %d\n", a, b)
+		if Da%D == 0 && Db%D == 0 {
+			// fmt.Printf("The solution is: a = %d, b = %d\n", a, b)
+			part1 += a*3 + b
+		}
+
+		x3, y3 = x3+10000000000000, y3+10000000000000
+		// Calculate the determinants for a and b
+		Da = det(x3, x2, y3, y2)
+		Db = det(x1, x3, y1, y3)
+		// Solve for a and b using Cramer's rule
+		a = Da / D
+		b = Db / D
+
+		if Da%D == 0 && Db%D == 0 {
+			// fmt.Printf("The solution is: a = %d, b = %d\n", a, b)
 			part2 += a*3 + b
-		} else {
-			fmt.Println("The solution is incorrect.")
 		}
 	}
 
