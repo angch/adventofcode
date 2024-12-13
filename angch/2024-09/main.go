7a0d0d34442862b3a29df54b118689956478b042
Ang Chin Han <ang.chin.han@gmail.com>
2024-12-13 12:40:33 +0800

    refac(angch/2024-09): Better structures

--- angch/2024-09/main.go
+++ angch/2024-09/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
 	"time"
 )
 
@@ -12,6 +14,26 @@ type diskstruct struct {
 	id  int
 	len int
 	pos int
+}
+
+func visualize(spans *list.List) {
+	fmt.Println("\n------------------")
+	for e := spans.Front(); e != nil; e = e.Next() {
+		v := e.Value.(*diskstruct)
+		fmt.Print("(", v.id, ":", v.len, ") ")
+	}
+	fmt.Println("\n")
+	for e := spans.Front(); e != nil; e = e.Next() {
+		v := e.Value.(*diskstruct)
+		for range v.len {
+			if v.id == -1 {
+				fmt.Print(".")
+			} else {
+				fmt.Print(v.id)
+			}
+		}
+	}
+	fmt.Println()
 }
 
 func day9(file string) (part1, part2 int) {
@@ -22,69 +44,101 @@ func day9(file string) (part1, part2 int) {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
-	spans := []diskstruct{}
+	spans := list.New()
+	spans2 := make([]*diskstruct, 0, 100)
+
 	files := 0
+
+	id := 0
 	for scanner.Scan() {
 		t := scanner.Text()
-		id := 0
 		for i := 0; i < len(t); i++ {
-			s := diskstruct{
-				id:  id,
-				len: int(t[i] - '0'),
-			}
-			spans = append(spans, s)
-			i++
-			if i < len(t) {
-				s := diskstruct{
-					id:  -1,
-					len: int(t[i] - '0'),
+			l := int(t[i] - '0')
+			if l > 0 {
+				s := &diskstruct{
+					id:  id,
+					len: l,
 				}
-				spans = append(spans, s)
+				s2 := *s
+				spans.PushBack(s)
+				spans2 = append(spans2, &s2)
+			}
+			i++
+
+			if i < len(t) {
+				l = int(t[i] - '0')
+				if l > 0 {
+					s := &diskstruct{
+						id:  -1,
+						len: l,
+					}
+					s2 := *s
+					spans.PushBack(s)
+					spans2 = append(spans2, &s2)
+				}
 			}
 			id++
 		}
 		files = id
 	}
-	spans2 := make([]diskstruct, len(spans))
-	copy(spans2, spans)
+	// spans2 := make([]diskstruct, spans.Len())
+	// for e := spans.Front(); e != nil; e = e.Next() {
+	// 	v := e.Value.(*diskstruct)
+	// 	fmt.Print("(", v.id, ":", v.len, ") ")
+	// }
+	// fmt.Println()
 
 	for {
-		l, r := 0, len(spans)-1
-		for ; l < r && spans[l].id != -1; l++ {
+		l, r := spans.Front(), spans.Back()
+
+		i := 0
+		for ; l != nil && l.Value.(*diskstruct).id != -1; l = l.Next() {
+			i++
 		}
-		for ; l < r && spans[r].id == -1; r-- {
+		j := spans.Len()
+		for ; r != nil && r.Value.(*diskstruct).id == -1; r = r.Prev() {
+			j--
 		}
-		if l >= r {
+		if i >= j {
 			break
 		}
 
-		spans[l].id = spans[r].id
-		if spans[l].len > spans[r].len {
-			spaceleft := spans[l].len - spans[r].len
-			spans[l].len = spans[r].len
-			spans = append(spans[:l+1], append([]diskstruct{{id: -1, len: spaceleft}}, spans[l+1:]...)...)
-			spans = append(spans[:r+1], spans[r+2:]...)
-		} else if spans[l].len < spans[r].len {
-			spans[r].len -= spans[l].len
+		lv := l.Value.(*diskstruct)
+		rv := r.Value.(*diskstruct)
+		lv.id = rv.id
+		if lv.len > rv.len {
+			spaceleft := lv.len - rv.len
+			lv.len = rv.len
+			spaceleftStruct := &diskstruct{id: -1, len: spaceleft}
+			spans.InsertAfter(spaceleftStruct, l)
+			spans.Remove(r)
+		} else if lv.len < rv.len {
+			rv.len -= lv.len
 		} else {
-			spans = append(spans[:r], spans[r+1:]...)
+			spans.Remove(r)
 		}
-		if spans[len(spans)-1].id == -1 {
-			spans = spans[:len(spans)-1]
+
+		last := spans.Back()
+		if last.Value.(*diskstruct).id == -1 {
+			spans.Remove(last)
 		}
+
+		// visualize(spans)
 	}
 
 	// fmt.Println(spans)
 	blockn := 0
-	for _, v := range spans {
+	for e := spans.Front(); e != nil; e = e.Next() {
+		v := e.Value.(*diskstruct)
 		for range v.len {
 			part1 += blockn * v.id
 			blockn++
 		}
 	}
 
-	idx := make(map[int]*diskstruct)
-	fsm := []diskstruct{}
+	// idx := make(map[int]*diskstruct)
+	idx := make([]*diskstruct, id)
+	fsm := []*diskstruct{}
 	blockn = 0
 	for _, v := range spans2 {
 		v.pos = blockn
@@ -93,8 +147,9 @@ func day9(file string) (part1, part2 int) {
 			fsm = append(fsm, v)
 		} else {
 			v2 := v
-			idx[v.id] = &v2
+			idx[v.id] = v2
 		}
+		// visualize(spans2)
 	}
 
 	for i := files - 1; i >= 0; i-- {
@@ -121,6 +176,15 @@ func day9(file string) (part1, part2 int) {
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	logperf := false
+	if logperf {
+		pf, _ := os.Create("default.pgo")
+		err := pprof.StartCPUProfile(pf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer pf.Close()
+	}
 	t1 := time.Now()
 	part1, part2 := day9("test.txt")
 	fmt.Println(part1, part2)
@@ -128,5 +192,9 @@ func main() {
 		log.Fatal("Test failed ", part1, part2)
 	}
 	fmt.Println(day9("input.txt"))
+	if logperf {
+		pprof.StopCPUProfile()
+	}
+
 	fmt.Println("Elapsed time:", time.Since(t1))
 }
